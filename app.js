c16125d30040f6336ff31ed7c9830add68316039
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2023-12-13 14:33:53 +0100

    fikset skru av bevegelighet ved å ikke kunne skru det på igjen uten å refreshe siden. bento oppsett i regler under om oss. startet oppdeling og refaktorering av css. logo i header

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
-		// console.log(entry);
-		if (entry.isIntersecting) {
-			entry.target.classList.add("show");
-		} else {
-			entry.target.classList.remove("show");
-		}
+		if (entry.isIntersecting) entry.target.classList.add("show");
+		else entry.target.classList.remove("show");
 	});
 });
 
-let motionTrigger = true;
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-function motionSwitch() {
-	if (motionTrigger) {
-		document.getElementById("reduserBevegelighet").innerHTML = "Skru på bevegelighet";
-		hiddenElements.forEach((el) => el.classList.add("show"));
-		hiddenElements.forEach((el) => el.classList.remove("hidden"));
-	} else {
-		document.getElementById("reduserBevegelighet").innerHTML = "Reduser bevegelighet";
-		hiddenElements.forEach((el) => el.classList.remove("show"));
-		hiddenElements.forEach((el) => el.classList.add("hidden"));
-		hiddenElements[0].classList.add("show");
-	}
-
-	motionTrigger = !motionTrigger;
+function motionSwitch(elem) {
+	hiddenElements.forEach((el) => el.classList.add("show"));
+	hiddenElements.forEach((el) => el.classList.remove("hidden"));
+	elem.remove();
 }
 
 function goToElement(element) {
